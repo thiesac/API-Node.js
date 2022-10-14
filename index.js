cf0c642ada3424b5bb93978fd03813923c568c33
Thiesa <thiesa.c@gmail.com>
2022-10-14 16:28:33 -0400

    feat: criacao de rota que calcula media de idades

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const server = express();
 server.use(express.json());
 
 const arrayPeople = [];
+const newArray = [];
+let sum = 0;
 
 //cadastra as pessoas e as insere no array
 server.post("/adicionar-pessoa", (req, res) => {
@@ -40,6 +42,34 @@ server.get("/qtd-mulheres", (req, res) => {
     }
   }
   return res.json(`Há ${counter} mulheres na lista`);
+});
+
+server.get("/media-idade-homens", (req, res) => {
+  for (let i = 0; i < arrayPeople.length; i++) {
+    if (arrayPeople[i].sexo === "masculino") {
+      sum += arrayPeople[i].idade;
+      newArray.push(arrayPeople[i].idade);
+    }
+  }
+  return res.json(
+    `A média de idade dos homens da lista é: ${(sum / newArray.length).toFixed(
+      0
+    )} anos`
+  );
+});
+
+server.get("/media-idade-mulheres", (req, res) => {
+  for (let i = 0; i < arrayPeople.length; i++) {
+    if (arrayPeople[i].sexo === "feminino") {
+      sum += arrayPeople[i].idade;
+      newArray.push(arrayPeople[i].idade);
+    }
+  }
+  return res.json(
+    `A média de idade das mulheres da lista é: ${(
+      sum / newArray.length
+    ).toFixed(0)} anos`
+  );
 });
 
 server.listen(3000);
