84d177eed58aa012c5eb5ef89a9ee45f6f4a687f
Thiesa <thiesa.c@gmail.com>
2022-10-14 12:58:41 -0400

    feat: criando cadastro de convidados

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const server = express();
+server.use(express.json());
+
+const arrayPeople = [];
+
+//cadastra as pessoas e as insere no array
+server.post("/adicionar-pessoa", (req, res) => {
+  const person = {
+    nome: req.body.nome,
+    idade: req.body.idade,
+    sexo: req.body.sexo,
+  };
+  arrayPeople.push(person);
+
+  return res.json(arrayPeople);
+});
+
+//retorna a quantia de homens no array
+server.get("/qtd-homens", (req, res) => {
+  let counter = 0;
+  const findMen = arrayPeople.map((man) => man.sexo);
+
+  for (let i = 0; i < findMen.length; i++) {
+    if (findMen[i] === "masculino") {
+      counter++;
+    }
+  }
+  return res.json(`Há ${counter} homens na lista`);
+});
+
+//retorna a quantia de mulheres no array
+server.get("/qtd-mulheres", (req, res) => {
+  let counter = 0;
+  const findWomen = arrayPeople.map((woman) => woman.sexo);
+
+  for (let i = 0; i < findWomen.length; i++) {
+    if (findWomen[i] === "feminino") {
+      counter++;
+    }
+  }
+  return res.json(`Há ${counter} mulheres na lista`);
+});
+
+server.listen(3000);
